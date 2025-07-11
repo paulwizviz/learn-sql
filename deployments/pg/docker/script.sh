422db3d29fdce102f355764e13b7773a92930d8d
Paul Sitoh <paulwizviz@gmail.com>
2025-07-11 22:38:00 +0100

    Add deployment examples
    - PostgreSQL single instance docker deployment
    
    Signed-off-by: Paul Sitoh <paulwizviz@gmail.com>

---
+++ deployments/pg/docker/script.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+if [ "$(basename $(realpath .))" != "learn-sql" ]; then
+    echo "You are outside the scope of the project"
+    exit 0
+fi
+
+export PGADMIN_DEFAULT_EMAIL=admin@psql.email
+export PGADMIN_DEFAULT_PASSWORD=admin
+
+export POSTGRES_USER=postgres
+export POSTGRES_PASSWORD=postgres
+
+export PSQL_VER=17.3-alpine3.20
+export PGADMIN_VER=9.0.0
+
+export NETWORK=learn-postgress_psql
+
+COMMAND="$1"
+SUBCOMMAND="$2"
+
+function single(){
+    local cmd=$1
+    case $cmd in
+        "start")
+            docker compose -f ./deployments/pg/docker/single/docker-compose.yaml up
+            ;;
+        "stop")
+            docker compose -f ./deployments/pg/docker/single/docker-compose.yaml down
+            ;;
+        "clean")
+            docker compose -f ./deployments/pg/docker/single/docker-compose.yaml down
+            if [ -d ./deployments/pg/docker/single/dbfiles ]; then
+                rm -rf ./deployments/pg/docker/single/dbfiles
+            fi
+            ;;
+        *)
+            echo "Usage $0 single [clean | start | stop]"
+            ;;
+    esac
+}
+
+case $COMMAND in
+    "single")
+        single $SUBCOMMAND
+        ;;
+    *)
+        echo "Usage: $0 [single]"
+        ;;
+esac
